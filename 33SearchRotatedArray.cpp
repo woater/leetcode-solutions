3ea9352bbdde31bb958b48904e43643465f0de2e
Yujie <arieswhynotfight@gmail.com>
2020-07-11 10:28:59 +0800

    add question analyzation

--- 33SearchRotatedArray.cpp
+++ 33SearchRotatedArray.cpp
@@ -1,5 +1,14 @@
 #include <vector>
 using namespace std;
+// Demand:
+// given an element search it in O(logn) time
+
+// Solution:
+// binary search
+// how to deal with rotation ?
+//  in each search, compare middle element with it's two negithbors
+//  if not ascending order
+//  then this is the pivot
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
