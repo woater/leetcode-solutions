ec84ddae1ee7658a71df50edd3f2488f9f078ec0
Yujie <arieswhynotfight@gmail.com>
2020-07-11 12:23:44 +0800

    33rd v3, add better ans

--- 33SearchRotatedArray.cpp
+++ 33SearchRotatedArray.cpp
@@ -97,3 +97,37 @@ public:
         }
     }
 };
+class Solution1 {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.empty())
+            return -1;
+        int begin = 0;
+        int end = nums.size() - 1;
+
+        while(begin <= end) {
+            int mid = (begin + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[begin] <= target && nums[mid] > target) {
+                    end = mid - 1;
+                }
+                else {
+                    begin = mid + 1;
+                }
+            }
+            else {
+                if (nums[end] >= target && nums[mid] < target) {
+                    //
+                    begin = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
