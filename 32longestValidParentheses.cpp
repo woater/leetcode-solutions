bbb7bc1a8f8403efc93dba0d35d3b569b9cbc9b4
Yujie <arieswhynotfight@gmail.com>
2020-07-13 12:03:56 +0800

    32nd, add instruction and basic solution thought

---
+++ 32longestValidParentheses.cpp
@@ -0,0 +1,18 @@
+//Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/longest-valid-parentheses
+#include <string>
+#include <vector>
+
+//Solution:
+//  Base solution: Do parentheses matching starting at each position
+//                  then count the maximum anser.
+//  Better solution: Dynamic programing
+//  
+using namespace std;
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+    }
+};
