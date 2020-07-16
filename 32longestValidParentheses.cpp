efa8050a239d97e8300029fb259be618279785cc
Yujie <arieswhynotfight@gmail.com>
2020-07-16 21:46:41 +0800

    32nd, v1.2, solve index error

--- 32longestValidParentheses.cpp
+++ 32longestValidParentheses.cpp
@@ -14,35 +14,41 @@ using namespace std;
 class Solution {
 public:
     int longestValidParentheses(string s) {
+        if (s.empty())
+            return 0;
         //DP array initialize
         int** dp = new int*[s.size()];
         for (int i = 0; i < s.size(); i++) {   
             dp[i] = new int[s.size()];
             memset(dp[i], 0, s.size() * sizeof(int));
         }
-        for (int i = 0; i < s.size(); i++) {
-            dp[i][i] = (s[i] == '(' ? '(' : ')');
-        }
+        // 无效代码，检查到这里很慢，基本没注意
+        // for (int i = 0; i < s.size(); i++) {
+        //     dp[i][i] = (s[i] == '(' ? '(' : ')');
+        // }
         int maxLength = 0;
         //Iteration
         for (int l = 1; l < s.size(); l+=2) {    //set step to 2,cause valid length is odd
-        cout << "l: " << l << endl; 
+        cout << "\nlength: " << l+1 << endl; 
             for (int i = 0; i < s.size() - l; i++) {
+                cout << i << "." << endl;
                 int j = i + l;
                 //  (dp[i][i+l/2] == 1 && dp[i+l/2+1][j]==1) can't conclude all situation, cause connect point may not be at middle
-                if (s[i] == '(' && s[j] == ')' && (l==1 || (l > 1 && dp[i+1][j-1] == 1))) {
+                if (s[i] == '(' && s[j] == ')' && (l==1 || (l > 1 && dp[i+1][j-1] == 1))) { // (.*)
                     dp[i][j] = 1;
-                    maxLength = l+1;
+                    maxLength = l+1;\
                 }
-                else {  // iterate over all possible connection
-                    for (int subL = 1; subL < l; subL+=2) {
-                        if (dp[i][subL] == 1 && dp[subL+1][j] == 1) {
+                else {  // iterate over all possible connection ().*
+                    for (int subL = 1; subL < l; subL++) {
+                        cout << "dp[" << i << "][" << subL << "]:dp[" << subL+1 << "][" << j << "]  " << dp[i][i+subL] << ":" << dp[i+subL+1][j] << endl;
+                        // index error
+                        if (dp[i][i + subL] == 1 && dp[i+subL+1][j] == 1) {
                             dp[i][j] = 1;
                             maxLength = l+1;
-                            break;
                         }
                     }
                 }
+                cout << "dp[" << i << "][" << j << "]: " << dp[i][j] << endl;
             }
         cout << "maxlength: " << maxLength << endl;
         }
@@ -54,7 +60,11 @@ int main()
 {
     //string s = "()()()()";
     //string s = "((()))";
-    string s = "()(())((";
+    //string s = "()(())((";
+    //string s;
+    //string s = ")()())";
+    string s = ")(()())";
+    //string s = ")(((((()())()()))()(()))(";
     cout << Solution().longestValidParentheses(s);
     return 0;
 }
